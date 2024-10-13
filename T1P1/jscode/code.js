830ab49ceac51b023722a7c8e285f5d662e87cee
alcides94 <alcidesquinterosugarte94@gmail.com>
2024-10-13 23:42:35 +0200

    subida de archivos con correccion de tp1

--- T1P1/jscode/code.js
+++ T1P1/jscode/code.js
@@ -44,7 +44,8 @@ function ejercicio3(){
         if (num > 0) {
             array.push(num)
         };
-    } while (num > 0);
+        //Si meto una cadena cuando pides numero en el apartado A, el programa no funciona como se pide. Debes controlar eso -> es con el -> isNaN
+    } while (num > 0 || isNaN(num)); // 
     // B 
     let mostrar=array.join(" ")
     console.log(mostrar);
@@ -61,6 +62,12 @@ function ejercicio3(){
     let num2;
     let salir=false;
     do {
+        /**
+         * D) - TRES variables para hacer un DO..WHILE que pida un numero positivo??? Por qué no usas parseInt??
+         * 
+         * Aqui controlo que no ingrese numeros decimales
+         * 
+         */
         num1=prompt("Ingrese un numero entero POSITIVO");
         num2=Math.floor(num1);
 
@@ -71,6 +78,12 @@ function ejercicio3(){
 
 
     // E 
+
+    /**
+     * 
+     * E) Líneas 80 y 83: repites el mismo código en ambas ramas de la estructura IF..ELSE Eso significa que ese código debe ir fuera de la estructura (y te ahorras el ELSE)
+     * 
+     */
     let primera=null;
     let ultima=0;
     let contador=0;
@@ -78,6 +91,8 @@ function ejercicio3(){
         if (array[i]==num1){
             if (primera == null){
                 primera=i;
+            
+            // aca me debo ahorrar estas lineas
                 ultima=i;
             }else{
                 ultima=i;
@@ -116,21 +131,35 @@ function ejercicio4(){
     }
     console.log(matriz);
     //B
+    /**
+     * 
+     * (0,75 punto) Muestra por consola el contenido de matriz respetando el número
+    de filas y columnas. Es decir, todos los valores de una fila deben salir en
+    horizontal separados por espacios y debajo, la siguiente fila. Además, todos
+    los valores deben ocupar 2 espacios. Si un valor ocupa solo un espacio, ponle
+    un 0 delante.
 
-    
-    let mostr=matriz.map(function(elemento){
-        elemento.map(function(ele){
+     * B) * No muestras la matriz como se pide- Aparte, la función MAP devuelve un array.
+     *  SI no vas a usar ese array, usa FOREACH, no MAP
+     *
+     * //CORREGIDO
+     */
+    let numeros;
+    matriz.forEach(function(elemento){
+        numeros=" ";
+        elemento.forEach(function(ele){
             if (ele<10){
-                return ele="0"+ele;
-            }else {
-            return ele;
+                ele="0"+ele;
             }
+            numeros=numeros+" "+ele;    
         })
-        return elemento;
+        
+        console.log (numeros);
     })
 
 
     //C
+    
     let suma=0;
     matriz.forEach(function(elemento){
         elemento.forEach(function(ele){
@@ -140,6 +169,13 @@ function ejercicio4(){
     console.log ("La suma es: "+suma);
     
     //D
+
+    /**
+     * 
+     * 
+     * 
+     */
+
     let copia=matriz.map(function(elemento){
         elemento.map(function(ele){
             if (ele>10){
@@ -153,6 +189,7 @@ function ejercicio4(){
     });
     console.log (copia);
 
-
+    //E
+    
 
 }
