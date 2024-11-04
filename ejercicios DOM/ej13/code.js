935a2fd2852daeab23b542faeb38b6253301e649
alcides94 <alcidesquinterosugarte94@gmail.com>
2024-11-05 00:40:25 +0100

    suida de ejercicios 13 y 14

--- ejercicios DOM/ej13/code.js
+++ ejercicios DOM/ej13/code.js
@@ -6,3 +6,24 @@ el mensaje cambia a “Ocultar”. Si se pulsa sobre ese mensaje, el elemento vu
 estado original (contenido oculto y mensaje “Mostrar”).
 
  */
+
+window.onload=()=>{
+    let mostrar=document.getElementsByTagName("h4")[0];
+    let ocultar=document.getElementsByTagName("h4")[1];
+    let parrafo=document.querySelector("p");
+    console.log(parrafo);
+
+    mostrar.onclick=()=>{
+        mostrar.style.setProperty("display","none");
+        ocultar.style.setProperty("display","block");
+        parrafo.style.setProperty("display","block");
+        
+    }
+    ocultar.onclick=()=>{
+        mostrar.style.setProperty("display","block");
+        ocultar.style.setProperty("display","none");
+        parrafo.style.setProperty("display","none");
+        
+    }
+    
+}
--- ejercicios DOM/ej14/code.js
+++ ejercicios DOM/ej14/code.js
@@ -1,6 +1,27 @@
-windows.onload=function(){
-    let valor=document.getElementsByName("menu")[0];
-    let valor2=document.getElementsByName("subcategoria");
+window.onload=function(){
+    let menu=document.getElementById("menu");
+    let list = document.getElementsByTagName("li");
+    let sub=document.getElementsByClassName("subcategoria")[0];
+    
 
+    console.log(menu);
+    console.log(sub);
+   let listaAnterior;
+   let listaSeg; 
+   for (let element of list) {
+        
+     element.onclick=()=>{
+        if (listaAnterior!=undefined){
+            listaAnterior.style.setProperty("display","none");
+        }else{
+            console.log(element);
+            listaSeg=element.querySelector("ul");
+            console.log(listaSeg);
+            listaSeg.style.setProperty("display","block");
+        }
+    }
+    listaAnterior=listaSeg;
+   }
+    
     
 }
