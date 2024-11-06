04d789229fcfdd7261db0c58afb5d37615052017
alcides94 <alcidesquinterosugarte94@gmail.com>
2024-11-06 16:34:02 +0100

    jerciciso 16

---
+++ ejercicios DOM/ej16/code.js
@@ -0,0 +1,31 @@
+let timer;
+window.onload=function () {
+    let meter = document.getElementsByTagName("meter")[0];
+    let boton = document.getElementsByTagName("button")[0];
+   
+    
+    boton.onclick=function(){
+        
+        timer=setInterval(function(){
+            if(parseInt(meter.value)+1==meter.max){
+
+                boton.disabled=false;
+                console.log(boton);
+                
+
+                clearInterval(timer);
+               // meter.value=0;
+            }
+            else{
+                meter.value+=1;
+                boton.disabled=true;
+            }
+                
+
+            
+        },100);
+    }
+
+   
+    
+}
