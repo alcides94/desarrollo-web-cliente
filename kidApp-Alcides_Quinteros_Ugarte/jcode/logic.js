48786851e2c1c7a93b905a771df384905dbd03c8
alcides94 <alcidesquinterosugarte94@gmail.com>
2024-11-07 12:08:55 +0100

    subida de archivos de practica opcional

---
+++ kidApp-Alcides_Quinteros_Ugarte/jcode/logic.js
@@ -0,0 +1,2 @@
+let fondos = ["red","green","blue","yellow","orange","pink","lightblue","lightgreen"];
+let ccolores = ["#00FF48","#D4478E","#FFD100","#0070FF","#7AAA83","#385B66","#ED91BF","#95AD95"];
