1f4b0bb045107264955c32f46aca98c917253e16
alcides94 <alcidesquinterosugarte94@gmail.com>
2025-01-28 12:02:31 +0100

    objetos ejercicios

---
+++ json/ObjetosLiterales/datos.js
@@ -0,0 +1,74 @@
+window.onload=()=>{
+let tutor ={
+    nombre:"Tomas",
+    edad:12,
+    dni:"12345678F",
+    titulo:"Electricista"
+}
+
+let asignaturas=[
+    {
+        nombre:"JS",
+        curso:"DAW",
+        horas:200
+    },
+    {
+        nombre:"Diseño",
+        curso:"DAW",
+        horas:100
+    },
+
+    {
+        nombre:"PHP",
+        curso:"DAW",
+        horas:200
+    },
+    {
+        nombre:"Despliegue",
+        curso:"DAW",
+        horas:150
+    }
+
+]
+
+let alumno={
+    nombre:"Ariel",
+    edad:32,
+    ciclo:"DAW",
+    curso:2,
+    profesor: tutor,
+    listaAsignaturas:[
+        asignaturas[0],
+        asignaturas[1],
+        asignaturas[2]
+    ],
+    notas:[7,7,8]
+
+}
+
+//console.table(alumno);
+
+for (alu in alumno){
+
+    if (alu == "profesor"){    
+        for (let a in alu) {
+            console.log(Object.values(a)); 
+        }
+        
+        
+    }else{
+        console.log(alu + " "+ alumno[alu]);    
+    }
+    
+}
+
+let caja = document.querySelector("div");
+
+//console.log(Object.values(alumno) );
+
+
+
+
+
+
+}
